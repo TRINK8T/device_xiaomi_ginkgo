09409a9ac133572165745329f4a3a7ba3cbda490
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-25 23:50:54 +0530

    ginkgo: Build perf HAL interfaces from source

--- device.mk
+++ device.mk
@@ -313,6 +313,9 @@ PRODUCT_PACKAGES_DEBUG += \
     pwrstats_util
 
 # Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
